7064574d00d5341bdd960146faec2a66dbe1cf3d
turbocece23 <cesar.engineering@gmail.com>
2021-12-27 15:02:17 +0100

    sistemata indentazione

--- Ctris/tris.c
+++ Ctris/tris.c
@@ -3,7 +3,7 @@
 int X,O;
 //Se vittoria è 0 allora il gioco continua, altrimenti se è 1 allora uno dei due giocatori ha vinto
 int vittoria;
-int turnoFinito=0;
+int turnoFinito = 0;
 
 char uno = '_';
 char due = '_';
@@ -15,65 +15,61 @@ char set = '_';
 char ott = '_';
 char nov = '_';
 
-void stampaTabellone()
-{
+void stampaTabellone() {
 	printf("_%c_|_%c_|_%c_\n",uno,due,tre);
 	printf("_%c_|_%c_|_%c_\n",qua,cin,sei);
 	printf("_%c_|_%c_|_%c_\n\n",set,ott,nov);
 }
 
-int controllaVittoria(char uno,char due,char tre,char qua,char cin,char sei,char set,char ott,char nov)
-{
+int controllaVittoria(char uno,char due,char tre,char qua,char cin,char sei,char set,char ott,char nov) {
 	//Casi di vittoria
-	if((uno=='X' && due=='X' && tre=='X') || (uno=='O' && due=='O' && tre=='O')){
-	if(uno==due && due==tre){
+	if ((uno == 'X' && due == 'X' && tre == 'X') || (uno == 'O' && due == 'O' && tre == 'O')) {
+	if (uno == due && due == tre) {
 		printf("Hanno vinto: %c!\n",uno);
 		return 1;}}
 
-	if((qua=='X' && cin=='X' && sei=='X') || (qua=='O' && cin=='O' && sei=='O')){
-	if(qua==cin && cin==sei){
+	if ((qua == 'X' && cin == 'X' && sei == 'X') || (qua == 'O' && cin == 'O' && sei == 'O')) {
+	if (qua == cin && cin == sei) {
 		printf("Hanno vinto: %c!\n",qua);
 		return 1;}}
 
-	if((set=='X' && ott=='X' && nov=='X') || (set=='O' && ott=='O' && nov=='O')){
-	if(set==ott && ott==nov){
+	if ((set == 'X' && ott == 'X' && nov == 'X') || (set == 'O' && ott == 'O' && nov == 'O')) {
+	if (set == ott && ott == nov) {
 		printf("Hanno vinto: %c!\n",set);
 		return 1;}}
 	
-	if((uno=='X' && qua=='X' && set=='X') || (uno=='O' && qua=='O' && set=='O')){
-	if(uno==qua && qua==set){
+	if ((uno == 'X' && qua == 'X' && set == 'X') || (uno == 'O' && qua == 'O' && set == 'O')) {
+	if (uno == qua && qua == set) {
 		printf("Hanno vinto: %c!\n",uno);
 		return 1;}}
 	
-	if((due=='X' && cin=='X' && ott=='X') || (due=='O' && cin=='O' && ott=='O')){
-	if(due==cin && cin==ott){
+	if ((due == 'X' && cin == 'X' && ott == 'X') || (due == 'O' && cin == 'O' && ott == 'O')) {
+	if (due == cin && cin == ott) {
 		printf("Hanno vinto: %c!\n",due);
 		return 1;}}
 	
-	if((tre=='X' && sei=='X' && nov=='X') || (tre=='O' && sei=='O' && nov=='O')){	
-	if(tre==sei && sei==nov){
+	if ((tre == 'X' && sei == 'X' && nov == 'X') || (tre == 'O' && sei == 'O' && nov == 'O')) {	
+	if (tre == sei && sei == nov) {
 		printf("Hanno vinto: %c!\n",tre);
 		return 1;}}
 	
-	if((uno=='X' && cin=='X' && nov=='X') || (uno=='O' && cin=='O' && nov=='O')){
-	if(uno==cin && cin==nov){
+	if ((uno == 'X' && cin == 'X' && nov == 'X') || (uno == 'O' && cin == 'O' && nov == 'O')) {
+	if (uno == cin && cin == nov) {
 		printf("Hanno vinto: %c!\n",uno);
 		return 1;}}
 	
-	if((tre=='X' && cin=='X' && set=='X') || (tre=='O' && cin=='O' && set=='O')){
-	if(tre==cin && cin==set){
+	if ((tre == 'X' && cin == 'X' && set == 'X') || (tre == 'O' && cin == 'O' && set == 'O')) {
+	if (tre == cin && cin == set) {
 		printf("Hanno vinto: %c!\n",tre);
 		return 1;}}
 	return 0;
 }
 
-int controllaInserimento(char posto, char giocatore)
-{
+int controllaInserimento(char posto, char giocatore) {
 	//La cella selezionata è già di un altro giocatore
 	//Se <la cella non è vuota> E <la cella è diversa dal giocatore corrente>
-	if(posto!='_' || posto==giocatore)
-	{
-		printf("\n=== Questa cella non può essere occupata! ===\n\n");
+	if (posto != '_' || posto == giocatore) {
+		printf("\n == = Questa cella non può essere occupata!  == =\n\n");
 		return 1;
 	//La cella selezionata è già del giocatore
 	}
@@ -81,55 +77,60 @@ int controllaInserimento(char posto, char giocatore)
 }
 
 //Controlla che tutte le caselle siano state occupate e che nessuno dei giocatori abbia vinto
-void controllaPareggio(int vittoria)
-{
-	if((uno!='_' && due!='_' && tre!='_' && qua!='_' && cin!='_' && sei!='_' && set!='_' && ott!='_' && nov!='_') && vittoria==0)
-	{
-		printf("\n=== La partità è finita in parità, resetto il tabellone ===\n");
-		uno='_';
-		due='_';
-		tre='_';
-		qua='_';
-		cin='_';
-		sei='_';
-		set='_';
-		ott='_';
-		nov='_';
+void controllaPareggio(int vittoria) {
+	if ((uno != '_' &&
+		due != '_' &&
+		tre != '_' &&
+		qua != '_' &&
+		cin != '_' &&
+		sei != '_' &&
+		set != '_' &&
+		ott != '_' &&
+		nov != '_') &&
+		vittoria == 0) {
+		printf("\n == = La partità è finita in parità, resetto il tabellone  == =\n");
+		uno = '_';
+		due = '_';
+		tre = '_';
+		qua = '_';
+		cin = '_';
+		sei = '_';
+		set = '_';
+		ott = '_';
+		nov = '_';
 	}
 }
 
 //Riempi la casella corrispondente a quella scelta dall'utente e fai finire il turno
 //impostando turnoFinito a 1
-void segnaPosto(int posto, char giocatore)
-{
-	switch(posto)
-	{
+void segnaPosto(int posto, char giocatore) {
+	switch(posto) {
 		case 1:
-			if(controllaInserimento(uno,giocatore)==0){uno=giocatore; turnoFinito=1;}
+			if (controllaInserimento(uno,giocatore) == 0) {uno = giocatore; turnoFinito = 1;}
 			break;
 		case 2:
-			if(controllaInserimento(due,giocatore)==0){due=giocatore; turnoFinito=1;}
+			if (controllaInserimento(due,giocatore) == 0) {due = giocatore; turnoFinito = 1;}
 			break;
 		case 3:
-			if(controllaInserimento(tre,giocatore)==0){tre=giocatore; turnoFinito=1;}
+			if (controllaInserimento(tre,giocatore) == 0) {tre = giocatore; turnoFinito = 1;}
 			break;
 		case 4:
-			if(controllaInserimento(qua,giocatore)==0){qua=giocatore; turnoFinito=1;}
+			if (controllaInserimento(qua,giocatore) == 0) {qua = giocatore; turnoFinito = 1;}
 			break;
 		case 5:
-			if(controllaInserimento(cin,giocatore)==0){cin=giocatore; turnoFinito=1;}
+			if (controllaInserimento(cin,giocatore) == 0) {cin = giocatore; turnoFinito = 1;}
 			break;
 		case 6:
-			if(controllaInserimento(sei,giocatore)==0){sei=giocatore; turnoFinito=1;}
+			if (controllaInserimento(sei,giocatore) == 0) {sei = giocatore; turnoFinito = 1;}
 			break;
 		case 7:
-			if(controllaInserimento(set,giocatore)==0){set=giocatore; turnoFinito=1;}
+			if (controllaInserimento(set,giocatore) == 0) {set = giocatore; turnoFinito = 1;}
 			break;
 		case 8:
-			if(controllaInserimento(ott,giocatore)==0){ott=giocatore; turnoFinito=1;}
+			if (controllaInserimento(ott,giocatore) == 0) {ott = giocatore; turnoFinito = 1;}
 			break;
 		case 9:
-			if(controllaInserimento(nov,giocatore)==0){nov=giocatore; turnoFinito=1;}
+			if (controllaInserimento(nov,giocatore) == 0) {nov = giocatore; turnoFinito = 1;}
 			break;
 		default:
 			printf("\nOpzione invalida, scegli un numero da 1 a 9\n\n");
@@ -138,8 +139,7 @@ void segnaPosto(int posto, char giocatore)
 }
 
 
-int main()
-{
+int main() {
 	//Stampa iniziale del tabellone di gioco, questa parte non si ripeterà
 	printf("Tabella di gioco:\n\n");
 	stampaTabellone();
@@ -147,25 +147,23 @@ int main()
 	printf("Il giocatore 2: O\n\n");
 	
 	//Inizializzazione della variabile "booleana" vittoria e inizio partita
-	vittoria=0;
+	vittoria = 0;
 	//Il ciclo si ripeterà fintanto che la vittoria non viene raggiunta e la variabile rimane a 0
-	while(vittoria==0){
+	while (vittoria == 0) {
 
 		//Turno del giocatore 1
-		if(vittoria==0)
-		{
-			do
-			{
+		if (vittoria == 0) {
+			do {
 				printf("Giocatore 1, fai la tua scelta: ");
 				scanf("%d",&X);
 				
 				segnaPosto(X,'X');
 
-			}while(turnoFinito!=1);
+			} while(turnoFinito != 1);
 		}
 		
 		//Reimposta la variabile del turno
-		turnoFinito=0;
+		turnoFinito = 0;
 		//Controlla la situazione attuale del tabellone
 		controllaPareggio(vittoria);
 		
@@ -173,31 +171,27 @@ int main()
 		stampaTabellone();
 		
 		//Se la partita è stata vinta da uno dei giocatori, interrompi il ciclo
-		if(controllaVittoria(uno,due,tre,qua,cin,sei,set,ott,nov)==1)
-		{
+		if (controllaVittoria(uno,due,tre,qua,cin,sei,set,ott,nov) == 1) {
 			vittoria=1;
 		}
 		
 		//Turno del Giocatore 2
-		if(vittoria==0)
-		{
-			do
-			{
+		if (vittoria == 0) {
+			do {
 				printf("Giocatore 2, fai la tua scelta: ");
-				scanf("%d",&O);
+				scanf("%d", &O);
 				
-				segnaPosto(O,'O');
+				segnaPosto(O, 'O');
 
-			}while(turnoFinito!=1);
+			} while(turnoFinito != 1);
 		}
 		
-		turnoFinito=0;
+		turnoFinito = 0;
 		controllaPareggio(vittoria);
 		
 		stampaTabellone();
 		
-		if(controllaVittoria(uno,due,tre,qua,cin,sei,set,ott,nov)==1)
-		{
+		if (controllaVittoria(uno,due,tre,qua,cin,sei,set,ott,nov) == 1) {
 			vittoria=1;
 		}
 	}
